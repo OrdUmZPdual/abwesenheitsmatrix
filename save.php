6144ba2896fabfe9c9f7262f27e9accf34aff2f9
OrdUmZPdual <161029803+OrdUmZPdual@users.noreply.github.com>
2025-09-15 10:44:35 +0200

    Speicherungslogik für automatische Löschung nach 1 Jahr

--- save.php
+++ save.php
@@ -23,6 +23,18 @@ if (!is_dir($dir)) {
     mkdir($dir, 0755, true);
 }
 
+// Alte Dateien löschen (älter als 12 Monate)
+$jetzt = time();
+$einJahr = 365 * 24 * 60 * 60;
+    foreach (glob("$dir/abwesenheiten_*.json") as $datei) {
+    if (preg_match("/_(\d{4})-(\d{2})\.json$/", $datei, $matches)) {
+        $dateDatum = strtotime("{$matches[1]}-{$matches[2]}-01");
+        if ($dateDatum < strtotime("-12 months")) {
+            unlink($datei);
+        }
+    }
+}
+
 $filename = "$dir/abwesenheiten_{$bereich}_{$monat}.json";
 file_put_contents($filename, $data);
 echo json_encode(["status" => "success"]);
